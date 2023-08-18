16178358a655ea3064b875a044856b4349bf48a2
roshankc00 <120471898+roshankc00@users.noreply.github.com>
2023-08-18 15:48:19 +0545

    feat:added the menu and the score board

--- main.cpp
+++ main.cpp
@@ -13,10 +13,16 @@ private:
     sf::Music crash;
     sf::SoundBuffer buffer;
     sf::Sound sound;
+    sf::Texture texturemenu;
+    sf::Sprite spritemenu;
+
+    int n = 0;
+    bool showmenu = false;
     static int count;
     char enemyImages[5][100] = {"van.png", "taxi.png", "semi_trailer.png", "pickup_truck.png"};
 
 public:
+    // set the texture
     void set_Texture()
     {
         if (!music.openFromFile("background.mp3"))
@@ -27,7 +33,7 @@ public:
         {
             std::cout << "ERROR ERROR :" << std::endl;
         }
-    
+
         music.play();
         crash.play();
         crash.setVolume(0);
@@ -64,7 +70,7 @@ public:
     {
         if (sprite.getPosition().y < 490)
         {
-            sprite.move(sf::Vector2f(0, 10));
+            sprite.move(sf::Vector2f(0, 6));
             std::cout << "the x postion of the right  sprite is " << spriteenemy.getPosition().x << " " << spriteenemy.getPosition().y << std::endl;
             std::cout << "the y postion of the sprite right  is " << sprite.getPosition().y << std::endl;
         }
@@ -73,7 +79,7 @@ public:
     {
         if (sprite.getPosition().y > 100)
         {
-            sprite.move(sf::Vector2f(-10, 0));
+            sprite.move(sf::Vector2f(-6, 0));
 
             std::cout << "the x postion of the right  sprite is " << sprite.getPosition().x << std::endl;
             std::cout << "the y postion of the sprite right  is " << sprite.getPosition().y << std::endl;
@@ -87,6 +93,7 @@ public:
 
     void create_Enemy()
     {
+        n = 0;
         // no repetation
         textureenemy.setRepeated(false);
 
@@ -120,19 +127,25 @@ public:
 
     void move_Enemy()
     {
+
         if (sprite.getGlobalBounds().intersects(spriteenemy.getGlobalBounds()))
         {
-     music.pause();       
-     if(crash.getStatus() != sf::Music::Status::Playing){
+            while (n = 0)
+            {
+                music.pause();
+                if (crash.getStatus() != sf::Music::Status::Playing)
+                {
 
-        crash.setVolume(100);
-      crash.play();
-     }
-        crash.setVolume(100);
+                    crash.setVolume(100);
+                    crash.play();
+                }
+                crash.setVolume(100);
 
-  
-
-            std::cout << "game over";
+                std::cout << "game over";
+                count++;
+                n = 1;
+                std::cout << "count" << std::endl;
+            }
         }
         else
         {
@@ -153,12 +166,55 @@ public:
     {
         return spriteenemy;
     }
+
+    // menu stups
+
+    void setmenu()
+    {
+
+        // setting up the texture
+        texturemenu.setRepeated(true);
+
+        // loading the texture
+        if (!texturemenu.loadFromFile("menu2.jpg"))
+        {
+            std::cout << "Error in loading the page " << std::endl;
+        }
+        //  spritemenu.setPosition(0, 0);
+        spritemenu.setTexture(texturemenu);
+    }
+
+    sf::Sprite return_menu()
+    {
+        return spritemenu;
+    }
+
+    void togglemenu()
+    {
+        if (showmenu)
+        {
+            showmenu = false;
+        }
+        else
+        {
+            showmenu = true;
+        }
+    }
+
+    bool return_showmenu()
+    {
+        return showmenu;
+    }
 };
 
 int Game::count;
 
 int main()
 {
+    // show or hide the menu
+    bool show = false;
+    int score1 = 0;
+
     sf::RenderWindow window(sf::VideoMode(600, 600), "Parallax Example",
                             sf::Style::Titlebar | sf::Style::Close);
     window.setVerticalSyncEnabled(true);
@@ -168,7 +224,19 @@ int main()
     g1.set_Texture();
     g1.create_Enemy();
 
+    // here
+    g1.setmenu();
     // backgound logic
+
+    //   font
+    sf::Font font;
+    font.loadFromFile("Real Delight.ttf");
+
+    // Create a text
+    sf::Text text(std::to_string(score1), font);
+    text.setCharacterSize(30);
+    text.setStyle(sf::Text::Bold);
+    text.setFillColor(sf::Color::Red);
 
     sf::Texture texture;
     if (!texture.loadFromFile("roooad.png"))
@@ -199,6 +267,8 @@ int main()
     {
         sf::Event event;
         while (window.pollEvent(event))
+            score1++;
+        sf::Text text("Score :" + std::to_string(score1), font);
         {
             switch (event.type)
             {
@@ -219,17 +289,36 @@ int main()
                     break;
                 }
             }
+
+            if (event.type == sf::Event::MouseButtonPressed)
+            {
+                if (sf::Mouse::getPosition().x > 1025 && sf::Mouse::getPosition().x < 1122 && sf::Mouse::getPosition().y > 576 && sf::Mouse::getPosition().y < 672)
+                {
+                    std::cout << "done" << std::endl;
+                    show = true;
+                }
+            }
         }
 
         // I set an arbitrary value as the offset, you'd calculate this based on camera position
         parallaxShader.setUniform("offset", offset += clock.restart().asSeconds() / 2);
 
-        g1.move_Enemy();
-        window.clear(sf::Color::Red);
-        window.draw(sprite, &parallaxShader);
-        window.draw(g1.returnme());
-        window.draw(g1.return_Enemy());
+        if (show)
+        {
+
+            g1.move_Enemy();
+            window.clear(sf::Color::Red);
+            window.draw(sprite, &parallaxShader);
+            window.draw(g1.returnme());
+            window.draw(g1.return_Enemy());
+            window.draw(text);
+        }
+        else
+        {
+            window.draw(g1.return_menu());
+        }
         window.display();
     }
     return 0;
 }
+
